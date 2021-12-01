91c6aa834437446698ff04ca358a7bb9b97732f7
Francisco Escobar <jfescobar18@hotmail.com>
2021-12-01 07:19:14 +0000

    command fixed

--- custom-bash.sh
+++ custom-bash.sh
@@ -1,4 +1,4 @@
 # Compile firmware
-function zmk_build() {
-        west build -d build/firmware/"$1" -p -b nrfmicro_13 -- -DSHIELD="$1"
+function nrf_build() {
+        west build -d build/firmware/"$1" -p -b nrfmicro_"$2" -- -DSHIELD="$1"
 }
